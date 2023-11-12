81be9be2bae280bb6340acdbae3de2ec6ffd6857
nasifswalah <nasifswalah@gmail.com>
2023-11-12 12:40:54 +0530

    js assignment day2

---
+++ grade.js
@@ -0,0 +1,11 @@
+var readlineSync = require('readline-sync');
+
+let grade = parseInt(readlineSync.question('Enter your grade: '));
+
+
+if(grade>=60){
+    console.log('You are passed');
+}
+else{
+    console.log('You are not eligible for voting');
+}
